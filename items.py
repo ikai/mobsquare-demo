3a657428a69fc38ef729fae43833270bc06b36d0
Ikai Lan <ikai.lan@gmail.com>
2011-06-08 11:32:33 +0800

    Adding a decorator for requiring facebook login, basic store code

---
+++ items.py
@@ -0,0 +1,21 @@
+# This file specifies the various items that can be used in MobSquare.
+# It was much easier to do it this way than to persist them for dev purposes.
+# In a real application, it might make sense t
+
+weapons = {}
+armor = {}
+
+weapons["bat"] = {
+    "name" : "Baseball Bat",
+    "image_url" : "",
+    "level_req" : 1,
+    "damage_bonus" : 1,
+    "to_hit_bonus" : 0.1
+}
+
+armor["leather_jacker"] = {
+    "name" : "Leather Jacket",
+    "image_url" : "",
+    "level_req" : 1,
+    "armor_bonus" : 1
+}
